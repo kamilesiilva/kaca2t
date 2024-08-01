8c3b99c98e05aa947243ca21c0b0f8b6fdb34365
kamilesiilva <166833616+kamilesiilva@users.noreply.github.com>
2024-08-01 11:35:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const perguntas = [
+    {
+    enunciado: "Insira o enunciado da Pergunta 1",
+    alternativas: [
+    {
+    texto: "Alternativa 1",
+    afirmação: "Afirmação da alternativa 1"
+    },
+    {
+    texto: "Alternativa 2",
+    afirmação: "Afirmação da alternativa 2"
+    }
+    ]
+    },
+    function mostraAlternativa() {
+        for (const alternativa of perguntaAtual.alternativas)
+        {
+        const botaoAlternativas = document. createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.addEventListener("click", function () {
+            atual++;
+            mostraPergunta();
+            });
+        }
+        for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document. createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",
+        function () {
+        atual++;
+        mostraPergunta();
+        });
+        caixaAlternativas.appendChild(botaoAlternativas);
+        }
+        }
+    ]
