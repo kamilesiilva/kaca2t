023b8933192e22ff1f8f7d17d0ec2dbf346840dc
kamilesiilva <166833616+kamilesiilva@users.noreply.github.com>
2024-08-08 11:41:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
+<script src="script.js"></script>
+const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-    enunciado: "Insira o enunciado da Pergunta 1",
-    alternativas: [
-    {
-    texto: "Alternativa 1",
-    afirmação: "Afirmação da alternativa 1"
+        enunciado: "Pergunta 1",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
     },
     {
-    texto: "Alternativa 2",
-    afirmação: "Afirmação da alternativa 2"
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
     }
-    ]
-    },
-    function mostraAlternativa() {
-        for (const alternativa of perguntaAtual.alternativas)
-        {
-        const botaoAlternativas = document. createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        caixaAlternativas.appendChild(botaoAlternativas);
-        botaoAlternativas.addEventListener("click", function () {
+    function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        }
+        mostraPergunta();
+        function mostraAlternativas() {
+            for (const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = document. createElement("button");
+            
+            botaoAlternativas.textContent = alternativa;
+            caixaAlternativas.appendChild(botaoAlternativas);
+            }
+        }
+        function mostraAlternativa() {
+            for (const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = document. createElement("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click",function () {
             atual++;
             mostraPergunta();
             });
-        }
-        for (const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document. createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",
-        function () {
-        atual++;
-        mostraPergunta();
-        });
-        caixaAlternativas.appendChild(botaoAlternativas);
-        }
-        }
-    ]
+            caixaAlternativas.appendChild(botaoAlternativas);
+            }
+            }
